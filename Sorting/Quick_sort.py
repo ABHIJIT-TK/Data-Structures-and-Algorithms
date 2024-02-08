aa729bb62a79d04de7e5f83f44a037422dfd2287
ABHIJIT TK <101189245+ABHIJIT-TK@users.noreply.github.com>
2024-02-08 10:19:09 +0530

    Quick_sort.py

---
+++ Sorting/Quick_sort.py
@@ -0,0 +1,38 @@
+def partition(arr, low, high):
+    i = low - 1
+    pivot = arr[high]
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+quick_sort(arr, 0, len(arr) - 1)
+print("Sorted array:", arr)
+
+
+# another Method
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less_than_pivot = [x for x in arr[1:] if x <= pivot]
+        greater_than_pivot = [x for x in arr[1:] if x > pivot]
+        return quick_sort(less_than_pivot) + [pivot] + quick_sort(greater_than_pivot)
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+sorted_arr = quick_sort(arr)
+print("Sorted array:", sorted_arr)
